adabc9b942cb209b87d0301e1a571dca118f4a66
RvvcIm <i@rvvc.im>
2022-02-02 16:44:58 +0800

    fix

--- wdcli.sh
+++ wdcli.sh
@@ -8,7 +8,7 @@ WEBDAV_PASSWORD=password
 
 about()
 {
-  # Script Name: wdcli
+  # Script Name: wdcli.sh
   # Github: rvvcim/webdav-client
   # Author: RvvcIm  
   # License: MIT
@@ -58,9 +58,9 @@ move()
 
 copy()
 {
-    source_url="${WEBDAV_URL}${1}"
-    target_url="${WEBDAV_URL}${2}"
-    webdav -X COPY --header "Destination:${target_url}" "${source_url}"
+  source_url="${WEBDAV_URL}${1}"
+  target_url="${WEBDAV_URL}${2}"
+  webdav -X COPY --header "Destination:${target_url}" "${source_url}"
 }
 remove()
 {
@@ -70,48 +70,48 @@ remove()
 }
 
 if [ ! "$#" = 0 ]; then
-    INCMD="$1"; shift
+  INCMD="$1"; shift
 fi
 
 case ${INCMD} in
-    mkdir)
-        makedir "$@"
-    ;;
-    push)
-        upload "$@"
-    ;;
-    pull)
-        download "$@"
-    ;;
-    ls)
-        list "$@"
-    ;;
-    mv)
-        move "$@"
-    ;;
-    cp)
-        copy "$@"
-    ;;
-    rm)
-        remove "$@"
-    ;;
-    help)
-        about
-        printf "
-    Webdav Client\n\
-    Version %s \n\
-    help:\n\
-        push <file> <dest>     上传文件\n\
-        pull <file>            下载文件\n\
-        mkdir <path>           新建文件夹\n\
-        mv <file> <dest>       移动文件\n\
-        cp <file> <dest>       复制文件\n\
-        rm <file>              删除文件\n\
-        ls <path>              显示文件列表\n\n\
-        Tips: 除创建目录外，目录结尾均须带 / ,文件列表未作处理，为xml格式。
-    " "${VERSION}"
-    ;;
-    *) printf "无事发生，试试输入 wdcli help ？\n";;
+  mkdir)
+    makedir "$@"
+  ;;
+  push)
+    upload "$@"
+  ;;
+  pull)
+    download "$@"
+  ;;
+  ls)
+    list "$@"
+  ;;
+  mv)
+    move "$@"
+  ;;
+  cp)
+    copy "$@"
+  ;;
+  rm)
+    remove "$@"
+  ;;
+  help)
+    about
+    printf "
+  Webdav Client\n\
+  Version %s \n\
+  help:\n\
+      push <file> <dest>     上传文件\n\
+      pull <file>            下载文件\n\
+      mkdir <path>           新建文件夹\n\
+      mv <file> <dest>       移动文件\n\
+      cp <file> <dest>       复制文件\n\
+      rm <file>              删除文件\n\
+      ls <path>              显示文件列表\n\n\
+      Tips: 除创建目录外，目录结尾均须带 / ,文件列表未作处理，为xml格式。
+  " "${VERSION}"
+  ;;
+  *) printf "无事发生，试试输入 wdcli help ？\n";;
 esac
 
 exit 0
